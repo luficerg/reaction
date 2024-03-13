08d5b156a9908734a048a467d3a21bdb720f61fc
Luficer G <luficerg5@gmail.com>
2024-03-13 19:31:13 +0530

    this is project completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types'
 
 export default function TextForm(props){
 
-    const handleClick = () => {
+    const handleClickUpper = () => {
         console.log("Uppercase was clicked" )
         let newTExt = text.toUpperCase()
+        setText(newTExt)
+    }
+
+    const handleClickLower = () => {
+        console.log("Lowercase was clicked" )
+        let newTExt = text.toLowerCase()
         setText(newTExt)
     }
 
@@ -16,18 +22,32 @@ export default function TextForm(props){
 
     const [text, setText] = useState("Enter the text here")
     return(
-        <div>
-            <div className="mb-3">
+        <>
+
+        <div className="mb-3">
                 <h1>
                 <label htmlFor="text" className="form-label">{props.heading}</label>
                 </h1>
                 
                 <textarea className="form-control" id="text" rows="10" value = {text} onChange={handleOnChange}></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={handleClick}>
-                    Submit
+                <div>
+                <button className="btn btn-primary mx - 4" onClick={handleClickUpper}>
+                    TO upper case
+                </button>
+            
+                <button className="btn btn-primary" onClick={handleClickLower}>
+                    TO lower case
                 </button>
         </div>
+
+        <div className="container">
+            <h1>Your text Summary here</h1>
+            <p> {text.split(" ").length} words and{text.length} characters</p>
+            <p> {0.1 *text.split(" ").length}  s to read the document</p>
+            <p>{text.toLowerCase()}</p>
+        </div>
+        </>
     )
 }
 
